d0a8b52b7e2cfc43ed59035a5048740fe2c2d541
Landmaster <python0429@gmail.com>
2021-04-27 09:23:14 -0400

    fix ui on macs

--- screenshotdialog.cpp
+++ screenshotdialog.cpp
@@ -15,17 +15,17 @@ ScreenshotDialog::ScreenshotDialog(const QString &frbFilename, QWidget *parent) 
 {
     ui->setupUi(this);
     scene->setAxesVisible(false);
-    connect(ui->pushButton, &QPushButton::clicked, this, [&]() {browseFrb(ui->lineEdit, ui->checkBox);});
-    connect(ui->pushButton_2, &QPushButton::clicked, this, [&]() {browseFrb(ui->lineEdit_2, ui->checkBox_2);});
-    connect(ui->pushButton_3, &QPushButton::clicked, this, [&]() {browseFrb(ui->lineEdit_3, ui->checkBox_3);});
-    connect(ui->pushButton_4, &QPushButton::clicked, this, [&]() {browseFrb(ui->lineEdit_4, ui->checkBox_4);});
+    connect(ui->pushButton, &QPushButton::clicked, this, [&]() { browseFrb(ui->lineEdit, ui->checkBox); });
+    connect(ui->pushButton_2, &QPushButton::clicked, this, [&]() { browseFrb(ui->lineEdit_2, ui->checkBox_2); });
+    connect(ui->pushButton_3, &QPushButton::clicked, this, [&]() { browseFrb(ui->lineEdit_3, ui->checkBox_3); });
+    connect(ui->pushButton_4, &QPushButton::clicked, this, [&]() { browseFrb(ui->lineEdit_4, ui->checkBox_4); });
 
     connect(ui->checkBox, &QCheckBox::clicked, this, [&](bool checked) { ui->lineEdit->setEnabled(checked); });
     connect(ui->checkBox_2, &QCheckBox::clicked, this, [&](bool checked) { ui->lineEdit_2->setEnabled(checked); });
     connect(ui->checkBox_3, &QCheckBox::clicked, this, [&](bool checked) { ui->lineEdit_3->setEnabled(checked); });
     connect(ui->checkBox_4, &QCheckBox::clicked, this, [&](bool checked) { ui->lineEdit_4->setEnabled(checked); });
 
-    if(!frbFilename.isEmpty()) {
+    if (!frbFilename.isEmpty()) {
         QFileInfo info(frbFilename);
         QRegularExpression re("(\\d)$");
         QString baseWithoutNumber = info.baseName().replace(re, "");
@@ -83,16 +83,16 @@ void ScreenshotDialog::readBoardFile(const QString &filename, BoardFile &boardFi
         stream >> boardFile;
         if (stream.status() != QDataStream::Status::ReadCorruptData) {
             for (auto &square: boardFile.boardData.squares) {
-                if(square.positionX < rect.left()) {
+                if (square.positionX < rect.left()) {
                     rect.setLeft(square.positionX);
                 }
-                if(square.positionX + 64> rect.right()) {
+                if (square.positionX + 64 > rect.right()) {
                     rect.setRight(square.positionX + 64);
                 }
-                if(square.positionY < rect.top()) {
+                if (square.positionY < rect.top()) {
                     rect.setTop(square.positionY);
                 }
-                if(square.positionY + 64 > rect.bottom()) {
+                if (square.positionY + 64 > rect.bottom()) {
                     rect.setBottom(square.positionY + 64);
                 }
             }
@@ -131,39 +131,39 @@ void ScreenshotDialog::accept() {
     BoardFile boardFile3;
     BoardFile boardFile4;
     QRectF rect;
-    QVector<QString> filenames;
-    if(ui->checkBox->isChecked()) {
+    QStringList filenames;
+    if (ui->checkBox->isChecked()) {
         filename = ui->lineEdit->text();
         filenames += filename;
         readBoardFile(filename, boardFile, rect);
     }
-    if(ui->checkBox_2->isChecked()) {
+    if (ui->checkBox_2->isChecked()) {
         filename2 = ui->lineEdit_2->text();
         filenames += filename2;
         readBoardFile(filename2, boardFile2, rect);
     }
-    if(ui->checkBox_3->isChecked()) {
+    if (ui->checkBox_3->isChecked()) {
         filename3 = ui->lineEdit_3->text();
         filenames += filename3;
         readBoardFile(filename3, boardFile3, rect);
     }
-    if(ui->checkBox_4->isChecked()) {
+    if (ui->checkBox_4->isChecked()) {
         filename4 = ui->lineEdit_4->text();
         filenames += filename4;
         readBoardFile(filename4, boardFile4, rect);
     }
-    if(ui->checkBox->isChecked()) {
+    if (ui->checkBox->isChecked()) {
         makeScreenshot(filename, boardFile, rect);
     }
-    if(ui->checkBox_2->isChecked()) {
+    if (ui->checkBox_2->isChecked()) {
         makeScreenshot(filename2, boardFile2, rect);
     }
-    if(ui->checkBox_3->isChecked()) {
+    if (ui->checkBox_3->isChecked()) {
         makeScreenshot(filename3, boardFile3, rect);
     }
-    if(ui->checkBox_4->isChecked()) {
+    if (ui->checkBox_4->isChecked()) {
         makeScreenshot(filename4, boardFile4, rect);
     }
-    QMessageBox::information(this, "Screenshot", "Saved: \n" + filenames.toList().join('\n'));
+    QMessageBox::information(this, "Screenshot", "Saved: \n" + filenames.join('\n'));
     close();
 }
